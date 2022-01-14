c58ac40c34bd58ff7683dcc1b1f5d2da1b455153
Caio Lucena <32599946+Vallentinm@users.noreply.github.com>
2022-01-14 11:36:59 -0300

    eiisso

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include('con_banco.php');
- 
+ /* Aquela merda de notificação de tiver usuario em blanck ou errado*/
 if(empty($_POST['usuario']) || empty($_POST['senha'])) {
 	header('Location:index.php');
 	exit();
@@ -10,12 +10,14 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])) {
 $usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
  
+/* Aqui é o select no banco, lembra desse karalho por favor!!!*/
 $query = "SELECT * FROM patrimon_dados.usuario where nome = '{$usuario}' and senha = '{$senha}'";
  
 $result = mysqli_query($conexao, $query);
  
 $row = mysqli_num_rows($result);
- 
+ /*Valida se tem o usuario no banco e da a mensagem maravilhosa de usuario ou sneha invalidos
+*/
 if($row == 1) {
 	$_SESSION['usuario'] = $usuario;
 	header('Location: painel.php');
