329d61a7e974788004c51d5881e89ee1a1cc0998
Caio Lucena <32599946+Vallentinm@users.noreply.github.com>
2022-02-16 15:14:15 -0300

    commit_2

--- views/pag_cad_emp.php
+++ views/pag_cad_emp.php
@@ -9,6 +9,7 @@ session_start();
   <link rel="stylesheet" href="../assets/css/Padrao.css">
 </head>
 <body>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <div class="navbar">
   <a href="painel.php">Pagina Inicial</a>
   <a href="painf_inf_relat.php">Relatórios</a>
@@ -38,14 +39,26 @@ session_start();
       <label for="cnpj"><b>CNPJ</b></label><br>
       <input type="text" id="cnpj" maxlength="" placeholder="xx.xxx.xxx/xxxx-xx" name="cnpj" required/><br/>
 
+      <label for="cnpj"><b>IE</b></label><br>
+      <input type="text" id="IE" maxlength="" placeholder="xxx.xxx.xxx.xxx" name="IE" required/><br/>
+
       <label for="telefone"><b>Telefone de contato</b></label><br>
       <input type="tel" id="telefone" placeholder="(xx) xxxx-xxxx" name="telefone"/><br/>
+
+      <label for="cep"><b>CEP</b></label><br>
+      <input type="text" id="cep" placeholder="xxxx-xxxx" name="cep" maxlength="9" minlength="8" required/><br/>
 
       <div class="p">
       <label for="Endereco"><b><h2>Endereço</h2></b></label><br></div>
 
+      <label for="logradouro"><b>Logadouro</b></label><br>
+      <input type="text" id="logradouro" placeholder="logradouro" name="logradouro" required/><br/>
+
+      <label for="Numero"><b>Numero</b></label><br>
+      <input type="text" id="Numero" placeholder="Numero" name="Logadouro" required/><br/>
+
       <label for="cidade"><b>Cidade</b></label><br>
-      <input type="text" id="cidade" placeholder="Cidade" name="cidade" required/><br/>
+      <input type="text" id="cidade" placeholder="Numero" name="Numero" required/><br/>
 
       <label for="bairro"><b>Bairro</b></label><br>
       <input type="text" id="bairro" placeholder="Endereço" name="bairro" required/><br/>
@@ -80,11 +93,39 @@ session_start();
         <option value="SE">Sergipe</option>
         <option value="TO">Tocantins</option>
       </select>
-
-      <label for="cep"><b>CEP</b></label><br>
-      <input type="text" id="cep" placeholder="xxxx-xxxx" name="cep" required/><br/>
       <button type="submit" class="registerbtn">Registrar</button>
     </div>
   </form>	
+
+  <script type="text/javascript">
+	$("#cep").focusout(function(){
+		//Início do Comando AJAX
+		$.ajax({
+			//O campo URL diz o caminho de onde virá os dados
+			//É importante concatenar o valor digitado no CEP
+			url: 'https://viacep.com.br/ws/'+$(this).val()+'/json/unicode/',
+			//Aqui você deve preencher o tipo de dados que será lido,
+			//no caso, estamos lendo JSON.
+			dataType: 'json',
+			//SUCESS é referente a função que será executada caso
+			//ele consiga ler a fonte de dados com sucesso.
+			//O parâmetro dentro da função se refere ao nome da variável
+			//que você vai dar para ler esse objeto.
+			success: function(resposta){
+				//Agora basta definir os valores que você deseja preencher
+				//automaticamente nos campos acima.
+				$("#logradouro").val(resposta.logradouro);
+				$("#complemento").val(resposta.complemento);
+				$("#bairro").val(resposta.bairro);
+				$("#cidade").val(resposta.localidade);
+				$("#uf").val(resposta.uf);
+				//Vamos incluir para que o Número seja focado automaticamente
+				//melhorando a experiência do usuário
+				$("#numero").focus();
+			}
+		});
+	});
+</script>
+<script>
 </body>
 </html>
